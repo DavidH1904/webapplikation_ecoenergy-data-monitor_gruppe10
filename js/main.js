e23585498eaedb4d7d3dec4a063243ffeecbc37b
DavidH1904 <david.heuckmann@googlemail.com>
2015-09-17 23:04:02 +0200

    Nachricht

---
+++ js/main.js
@@ -0,0 +1,6 @@
+angular.module('app', ['ui.bootstrap']).controller('DropdownCtrl', function ($scope, $log) {
+  $scope.items = [
+    'The first choice!',
+    'And another choice for you.',
+    'but wait! A third!'
+  ]});
